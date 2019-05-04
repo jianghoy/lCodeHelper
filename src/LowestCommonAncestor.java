0af25cd67956fb482e75f4ab9820434fe86f285b
Jianghong Ying <YJianghong@gmail.com>
2019-05-04 17:45:18 -0400

    modified LowestCommonAncestor class -- still not working for now

--- src/LowestCommonAncestor.java
+++ src/LowestCommonAncestor.java
@@ -1,4 +1,44 @@
-package PACKAGE_NAME;
-
 public class LowestCommonAncestor {
-}
+
+        private int count = 0;
+        public TreeNode lowestCommonAncestorIII(TreeNode root,
+                                                TreeNode one, TreeNode two) {
+            // write your solution here
+            // if (root==null) return null;
+            //else if (one==null) return two;
+            //else if (two==null) return one;
+            //count=0;
+            //int[] count = new int[1];
+            TreeNode sol = lca(root, one, two);
+            return count >= 2 ? sol : null;
+            //return null;
+        }
+
+        private TreeNode lca(TreeNode root, TreeNode one, TreeNode two) {
+            if (root == null) {
+                return root;
+            }
+            if (root == one || root == two) {
+                count++;
+            }
+            TreeNode l = lca(root.left, one, two);
+            TreeNode r = lca(root.right, one, two);
+
+            if (root == one || root == two || (l != null && r != null)) {
+                return root;
+            } else {
+                return l != null ? l : r;
+            }
+
+        }
+
+        public TreeNode find(TreeNode root , int key){
+            if (root == null || root.key == key){
+                return root;
+            }
+            TreeNode solution = find(root.left,key);
+            return solution != null ? solution:find(root.right,key);
+        }
+    }
+
+
